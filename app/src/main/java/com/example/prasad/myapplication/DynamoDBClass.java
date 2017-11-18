63d4ea0019ce7af479fc8cc8cdf9aabccd0966d4
Prasad Bhagwat <prasad@172-16-207-234.DYNAPOOL.NYU.EDU>
2017-11-18 18:11:59 -0500

    adding dynamo db functionality

---
+++ app/src/main/java/com/example/prasad/myapplication/DynamoDBClass.java
@@ -0,0 +1,38 @@
+package com.example.prasad.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+//import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*
+/**
+ * Created by prasad on 11/18/17.
+ */
+public class DynamoDBClass extends AppCompatActivity {
+
+    CognitoCachingCredentialsProvider credentialsProvider;
+    AmazonDynamoDBClient ddbClient;
+    DynamoDBMapper mapper;
+    DynamoDBClass() {
+         this.credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),    /* get the context for the application */
+                "us-east-1_FRKZmmd4r",    /* Identity Pool ID */
+                Regions.US_EAST_1           /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
+        );
+         this.ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+        this.mapper = new DynamoDBMapper(ddbClient);
+
+
+    }
+
+    public void addItems(){
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/prasad/myapplication/StoreActivity.java
+++ app/src/main/java/com/example/prasad/myapplication/StoreActivity.java
@@ -3,11 +3,15 @@ package com.example.prasad.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+
 public class StoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
     }
 }
